61ed1ea63f644c37a742d80187147bd9f318bdee
tman16 <t_man_13@hotmail.com>
2025-12-05 14:59:59 +0000

    Update celestial.js

--- celestial.js
+++ celestial.js
@@ -672,7 +672,7 @@ Celestial.display = function(config) {
             context.fillText(sym[cfg.planets.symbolType], pt[0], pt[1]);            
           }
           //name
-          if (cfg.planets.names && id !== "sol" || cfg.planets.names && id !== "moon") {
+          if (cfg.planets.names && (id !== "sol" || id !== "lun")) {
             var name = p[cfg.planets.namesType];
             setTextStyle(cfg.planets.nameStyle);
             //context.direction = "ltr" || "rtl" ar il ir
